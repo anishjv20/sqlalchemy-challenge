9595205025c2fea7eecd62b2df17ef760bbb71ab
anishjv20 <anishjv20@gmail.com>
2020-08-17 23:57:13 -0400

    saving updates

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+import datetime as dt
+
 engine = create_engine("sqlite:///Resources/hawaii.sqlite", echo=False)
 
 Base = automap_base()
@@ -21,10 +23,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return (
-    f"Available API routes:"
-    f"/api/v1.0/precipitation"
-    f"/api/v1.0/stations"
-    f"/api/v1.0/<start>"
+    f"Available API routes:<br/>"
+    f"/api/v1.0/precipitation<br/>"
+    f"/api/v1.0/stations<br/>"
+    f"/api/v1.0/<start><br/>"
     f"/api/v1.0/<start>/<end>"
     )
 
@@ -32,4 +34,49 @@ def home():
 def precipitation():
     session = Session(engine)
     
-    results = session.query(Measurement.date
+    results = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    temp_values = []
+    for date, temp in results:
+        temp_dict = {}
+        temp_dict[date] = temp
+        temp_values.append(temp_dict)
+
+    return jsonify(temp_values)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    results = session.query(Station.name).all()
+
+    session.close()
+
+    station_names = list(np.ravel(results))
+
+    return jsonify(station_names)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+
+    # date = dt.date(2017,8,23) - dt.timedelta(days=365)
+
+    results = session.query(Measurement.date, Measurement.tobs).\
+                filter(Measurement.station=='USC00519281').\
+                filter(Measurement.date>'2016-08-24').all()
+    
+    session.close()
+
+    temp_obs = list(np.ravel(results))
+
+    return jsonify(temp_obs)
+
+@app.route("/api/v1.0/<start>")
+
+@app.route("/api/v1.0/<start>/<end>")
+
+if __name__ == "__main__":
+    app.run(debug=True)
